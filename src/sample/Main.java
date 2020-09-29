6347b4b582408fcd9c1e0b2b4226c9cb211f6d6b
jzygadlo <jan.zygadlo.0@gmail.com>
2020-09-29 17:13:50 +0200

    --

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,13 +12,15 @@ import java.util.List;
 public class Main implements RMIInterface {
 
     private static Main serverObject = null;
-    private static final String nextIp = "192.168.1.101";
-    private final int priority = 1;
-    private int coordinatorPriority;
     private static RMIInterface rmi = null;
+
+    private static final String nextIp = "192.168.1.101";
+    private final int priority = 2;
+
+    private int coordinatorPriority;
     private List<String[]> inithosts;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws RemoteException, NotBoundException, UnknownHostException, InterruptedException {
 
         Registry reg = null;
 
@@ -30,7 +32,8 @@ public class Main implements RMIInterface {
         }
 
         serverObject = new Main();
-        System.out.println("RMI server online");
+        System.out.println("RMI server online at ");
+        System.out.println(java.net.InetAddress.getLocalHost().getHostAddress());
 
         try {
             reg.rebind("server", UnicastRemoteObject.exportObject(serverObject, 0));
@@ -38,12 +41,34 @@ public class Main implements RMIInterface {
             System.out.println("ERROR: Failed to register the server object.");
             e.printStackTrace();
         }
+
+        /*
+        List<String[]> host = new LinkedList<>();
+        host.add(new String[]{Integer.toString(1), "192.168.1.100"});
+
+        Registry registry = LocateRegistry.getRegistry(nextIp, 1099);
+        rmi = (RMIInterface) registry.lookup("server");
+        rmi.sendElect(host);
+        */
     }
 
     @Override
     public void sendElect(List<String[]> host) throws InterruptedException, RemoteException, UnknownHostException, NotBoundException {
         receiveElect(host);
     }
+
+//    public void receiveElect(List<String[]> host) throws InterruptedException, RemoteException, UnknownHostException, NotBoundException {
+//        System.out.println("test 2");
+//        String message = "";
+//        for (String[] record : host) {
+//            message += record[0] + "\t" + record[1] + "\n";
+//        }
+//        System.out.println(message);
+//        host.add(new String[]{Integer.toString(2), "192.168.1.101"});
+//        Registry registry = LocateRegistry.getRegistry(nextIp, 1099);
+//        rmi = (RMIInterface) registry.lookup("server");
+//        rmi.sendElect(host);
+//    }
 
     public void receiveElect(List<String[]> host) throws InterruptedException, RemoteException, UnknownHostException, NotBoundException {
 
@@ -52,12 +77,18 @@ public class Main implements RMIInterface {
         String message = "Otrzymano sygnał ELECT z hostami: \n\n";
         message += "Priorytet\tIP\n";
 
+        System.out.print(message);
+        message="";
+
         for (String[] record : host) {
-            if (record[0] == Integer.toString(priority)) elected = true;
+            if (record[0].equals(Integer.toString(priority))) elected = true;
 
             message += record[0] + "\t" + record[1] + "\n";
 
         }
+
+        System.out.print(message);
+        message="";
 
         if (elected) {
 
@@ -71,6 +102,9 @@ public class Main implements RMIInterface {
             }
 
             message += "\ndo hosta " + nextIp + "\n";
+            System.out.println(message);
+
+            Thread.sleep(2000);
 
             Registry registry = LocateRegistry.getRegistry(nextIp, 1099);
             rmi = (RMIInterface) registry.lookup("server");
@@ -83,7 +117,6 @@ public class Main implements RMIInterface {
 
             List<String[]> newHost = host;
             newHost.add(new String[]{Integer.toString(priority), java.net.InetAddress.getLocalHost().getHostAddress()});
-            rmi.sendElect(newHost);
 
             message += "Wysłano sygnał ELECT o treści: \n\n";
             message += "Priorytet\tIP\n";
@@ -94,6 +127,13 @@ public class Main implements RMIInterface {
             }
 
             message += "\ndo hosta " + nextIp + "\n";
+            System.out.println(message);
+
+            Thread.sleep(2000);
+
+            Registry registry = LocateRegistry.getRegistry(nextIp, 1099);
+            rmi = (RMIInterface) registry.lookup("server");
+            rmi.sendElect(newHost);
 
         }
     }
@@ -105,8 +145,7 @@ public class Main implements RMIInterface {
 
     public void receiveCoordinator(List<String[]> host) throws InterruptedException, RemoteException, UnknownHostException, NotBoundException {
 
-        int maxPriority = 0;
-        String[] coordinatorRecord = new String[]{"", ""};
+        String[] coordinatorRecord = new String[]{"0", ""};
 
         String message = "Otrzymano sygnał COORDINATOR z hostami: \n\n";
         message += "Priorytet\tIP\n";
@@ -115,16 +154,16 @@ public class Main implements RMIInterface {
 
             message += record[0] + "\t" + record[1] + "\n";
 
-            if (Integer.parseInt(record[0]) > maxPriority) {
-                maxPriority = Integer.parseInt(record[0]);
+            if (Integer.parseInt(record[0]) > Integer.parseInt(coordinatorRecord[0])){
+                coordinatorRecord = new String[]{record[0], record[1]};
             }
         }
 
-        if (maxPriority != coordinatorPriority) {
+        if (Integer.parseInt(coordinatorRecord[0]) != coordinatorPriority) {
 
             message += "\nZ rekordów wybrano koordynatora: " + coordinatorRecord[0] + "\t" + coordinatorRecord[1] + "\n";
 
-            coordinatorPriority = maxPriority;
+            coordinatorPriority = Integer.parseInt(coordinatorRecord[0]);
 
             message += "Wysyłam sygnał COORDINATOR o treści: \n\n";
             message += "Priorytet\tIP\n";
@@ -136,15 +175,18 @@ public class Main implements RMIInterface {
             }
 
             message += "\ndo hosta " + nextIp + "\n";
+            System.out.println(message);
 
+            Thread.sleep(2000);
             Registry registry = LocateRegistry.getRegistry(nextIp, 1099);
             rmi = (RMIInterface) registry.lookup("server");
-            rmi.sendElect(host);
+            rmi.sendCoordinator(host);
 
         }
         else {
 
             message += "\nWszystkie węzły przyjęły wybranego koordynatora: " + coordinatorRecord[0] + "\t" + coordinatorRecord[1] + "\n";
+            System.out.println(message);
 
         }
     }
